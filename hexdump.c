aca263708211959c8a3e4e4be12b8ed922304e2c
Aaron <aaron@aaronkondziela.com>
2016-05-04 19:53:35 -0400

    Fixup for full-width last line

--- hexdump.c
+++ hexdump.c
@@ -13,7 +13,7 @@ void hexdump(const void* d, size_t size)
 		printf("%02X ", data[i]);
 		txt[i % 16] = (data[i] >= 0x20 && data[i] <= 0x7E) ? data[i] : '.';
 		if ((i+1) % 8 == 0 || i+1 == size) printf(" ");
-		if (i+1 == size) {
+		if (i+1 == size && (i+1) % 16 != 0) {
 			txt[(i+1) % 16] = '\0';
 			for (j = (i+1) % 16; j < 16; j++) printf("   ");
 			if ((i+1) % 16 <= 8) printf(" ");
